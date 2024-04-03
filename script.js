440b049c1ab317f8d55e1cf1437101e6a711a36b
dibmens <dibmens@gmail.com>
2024-04-03 23:43:39 +0300

    Create necessary functions for the game logic

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// Show current grid state, draw it into DOM, accept
-// player inputs in cells 
-(function GameBoard(){
+function Cell(){
+    let value = 0;
+    getValue = () => value; 
+    setValue = (player) => value = player;
+    return {getValue, setValue};
+}
+
+function GameBoard(){
     const grid = [];
     resetBoard = () => {
         grid.length = 0;
@@ -9,32 +14,38 @@
         }
     }
     updateBoard = (index,player) => grid[index].setValue(player);
-    getBoard = () => grid;
-    return {resetBoard, updateBoard, getBoard}
-})();
+    getBoard = () => grid.map( each => each.getValue());
+    return { resetBoard, updateBoard, getBoard}
+};
 
-// Populate the game grid, set and get cell's value
-function Cell(){
-    let value = 0;
-    getValue = () => value; 
-    setValue = (player) => value = player;
-    return {getValue, setValue};
-}
+function PlayerState(){
+    let player = 0;
+    switchPlayer = () => 
+        player == 0 ? player = 1 : player == 1 ? player = 2 : player = 1;
+    resetPlayer = () => player = 0;
+    getPlayer = () => player ; 
+    return {switchPlayer, resetPlayer, getPlayer};
+};
 
-// Run the game, switch turns, accept player inputs, determine
-//  win-condition
-// function GameController(){
-
-// }
-
-// Controls the UI representation of the game. Board, turns,
-// player inputs, win screen etc.
-// function ScreenController(){
+function AiLogic(){
+    moveAi = () => {
+        if (getBoard()[4] == 0){
+            updateBoard(4,2)
+        } else if(getBoard().includes(0) == false) {
+            console.log('RUN OUT OF FREE CELLS!');
+        // } else if() {
+        // if getPlayerMoves matches TWO elements in winMoves, make a winning move 
+        } else {
+            let random = () => Math.trunc(Math.random()*getPlayerMoves(0).length);
+            updateBoard(getPlayerMoves(0)[random()],2);
+        }
+    }
+    return {moveAi};
     
-// }1
+};
 
-(function WinCondition(){
-    const winners = [
+function WinCondition(){
+    const winMoves = [
         [0,1,2],
         [3,4,5],
         [6,7,8],
@@ -44,14 +55,26 @@ function Cell(){
         [0,4,8],
         [2,4,6]
     ];
-    getWinners = () => winners;
+    const playerMoves = [];
+    getPlayerMoves = (player) => {
+        playerMoves.length = 0;
+        getBoard().forEach((element, index) => 
+            element == player ? playerMoves.push(index) : 0);
+            return playerMoves;
+    };
+    getWinMoves = () => winMoves;
     checkWin = () => {
-        let filtered = [];
-        getBoard().forEach((cell, index) => cell.getValue() == 1 ? filtered.push(index));
-        return filtered;
+        let moves = getPlayerMoves(getPlayer());
+        getWinMoves().forEach(element => {
+            moves.includes(element[0])&&
+            moves.includes(element[1])&&
+            moves.includes(element[2]) ?
+            alert(`Player ${getPlayer()} wins at line ${element.toString()}`) :
+            console.log(`miss`);
+        });
+        
     }
-    checkDraw = () => {
+    return {getWinMoves};
+};
 
-    }
-    return {getWinners, checkWin, checkDraw};
-})();
+
