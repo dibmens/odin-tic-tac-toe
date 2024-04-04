26c847e7eb5d6c3380a35fb957fc4e9f1394db6c
dibmens <dibmens@gmail.com>
2024-04-04 18:17:32 +0300

    Complete opponent's AI logic

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Cell(){
     return {getValue, setValue};
 }
 
-function GameBoard(){
+(function GameBoard(){
     const grid = [];
     resetBoard = () => {
         grid.length = 0;
@@ -15,36 +15,50 @@ function GameBoard(){
     }
     updateBoard = (index,player) => grid[index].setValue(player);
     getBoard = () => grid.map( each => each.getValue());
-    return { resetBoard, updateBoard, getBoard}
-};
+})();
 
-function PlayerState(){
+(function PlayerState(){
     let player = 0;
     switchPlayer = () => 
         player == 0 ? player = 1 : player == 1 ? player = 2 : player = 1;
     resetPlayer = () => player = 0;
     getPlayer = () => player ; 
-    return {switchPlayer, resetPlayer, getPlayer};
-};
+})();
 
-function AiLogic(){
+(function AiLogic(){
+    let winningMove;
+    let findWinPattern = function(i1,i2,i3){
+        getWinMoves().forEach(element => {
+            if(
+                findMatch(element,i1)
+                && findMatch(element,i2)
+                && !findMatch(element,i3)
+                && getBoard()[i3] == 0
+                ){
+                winningMove = element[i3];
+            };
+        });
+        return winningMove;
+    }
     moveAi = () => {
+        winningMove = 0;
+        findWinPattern(0,1,2);
+        findWinPattern(1,2,0);
+        findWinPattern(2,0,1);
         if (getBoard()[4] == 0){
             updateBoard(4,2)
-        } else if(getBoard().includes(0) == false) {
+        } else if(!getBoard().includes(0)) {
             console.log('RUN OUT OF FREE CELLS!');
-        // } else if() {
-        // if getPlayerMoves matches TWO elements in winMoves, make a winning move 
+        } else if(winningMove && getBoard()[winningMove] == 0) {
+            updateBoard(winningMove,2);
         } else {
-            let random = () => Math.trunc(Math.random()*getPlayerMoves(0).length);
-            updateBoard(getPlayerMoves(0)[random()],2);
+            let pickRandom = () => Math.trunc(Math.random()*getPlayerMoves(0).length);
+            updateBoard(getPlayerMoves(0)[pickRandom()],2);
         }
-    }
-    return {moveAi};
-    
-};
+    } 
+})();
 
-function WinCondition(){
+(function WinCondition(){
     const winMoves = [
         [0,1,2],
         [3,4,5],
@@ -63,18 +77,16 @@ function WinCondition(){
             return playerMoves;
     };
     getWinMoves = () => winMoves;
-    checkWin = () => {
-        let moves = getPlayerMoves(getPlayer());
-        getWinMoves().forEach(element => {
-            moves.includes(element[0])&&
-            moves.includes(element[1])&&
-            moves.includes(element[2]) ?
-            alert(`Player ${getPlayer()} wins at line ${element.toString()}`) :
-            console.log(`miss`);
-        });
-        
+    findMatch = (winArray,index) => {
+        return getPlayerMoves(getPlayer()).includes(winArray[index]);
     }
-    return {getWinMoves};
-};
+    checkWin = () => {
+        getWinMoves().forEach(element => {
+            if(findMatch(element,0) && findMatch(element,1) && findMatch(element,2)){
+                alert(`Player ${getPlayer()} wins at line ${element.toString()}`)
+            }
+        });
+    }
+})();
 
 
